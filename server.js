e48c240136ba0febfa690f0dd850198850bd0f70
Ty the Fly Guy <tstreets@iu.edu>
2025-04-22 17:26:33 -0400

    Added static frontend

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", require("./api"));
+
+app.use(express.static(path.join(__dirname, "views")));
 
 app.listen(3003);
 
