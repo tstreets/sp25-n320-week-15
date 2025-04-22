fc396ad5ae3b426107f035ec635c9bfa68115ce4
Ty the Fly Guy <tstreets@iu.edu>
2025-04-22 17:39:41 -0400

    Testing server

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ app.use(express.json());
 app.use("/api", require("./api"));
 
 app.use(express.static(path.join(__dirname, "views")));
-app.use("/n320nodejs", express.static(path.join(__dirname, "views")));
+app.use(
+  "/n320nodejs",
+  function (req, res, next) {
+    console.log(req.url);
+    if (req.url.endsWith(".html")) {
+      res.redirect(req.url);
+    } else {
+      next();
+    }
+  },
+  express.static(path.join(__dirname, "views"))
+);
 
 app.listen(13883);
 
